d15b21d39e3cd05b07171b12176568cc8f568846
Oleg Dramaretskyi <dramarec@gmail.com>
2021-02-16 11:56:00 +0200

    finished styles

--- src/pages/signin/LoginPage.js
+++ src/pages/signin/LoginPage.js
@@ -3,27 +3,29 @@ import LoginPageStyled from './LoginPageStyled';
 
 const LoginPage = () => {
     return (
-        <div className="container">
-            <LoginPageStyled>
-                <h2 className="titleLoginPage">ВХОД</h2>
+        <LoginPageStyled className="sectionLoginPage">
+            <div className="container">
                 <div className="wrapLoginPage">
-                    <form className="formLoginPage">
-                        <label className="labelLoginPage">
-                            Логин *
-                            <input type="text" className="inputLoginPage" />
-                        </label>
-                        <label className="labelLoginPage">
-                            Пароль *
-                            <input type="text" className="inputLoginPage" />
-                        </label>
-                    </form>
+                    <div className="wrapForm">
+                        <h2 className="titleLoginPage">ВХОД</h2>
+                        <form className="formLoginPage">
+                            <label className="labelLoginPage">
+                                Логин *
+                                <input type="text" className="inputLoginPage" />
+                            </label>
+                            <label className="labelLoginPage">
+                                Пароль *
+                                <input type="text" className="inputLoginPage" />
+                            </label>
+                        </form>
+                    </div>
+                    <div className="wrapBtn">
+                        <button className="mainButton">Вход</button>
+                        <button className="secondButton">Регистрация</button>
+                    </div>
                 </div>
-                <div className="btnWrapLoginPage">
-                    <button className="mainButton">Вход</button>
-                    <button className="secondButton">Регистрация</button>
-                </div>
-            </LoginPageStyled>
-        </div>
+            </div>
+        </LoginPageStyled>
     );
 };
 
--- src/pages/signin/LoginPageStyled.js
+++ src/pages/signin/LoginPageStyled.js
@@ -1,19 +1,14 @@
 import styled from 'styled-components';
 
-const LoginPageStyled = styled.div`
-    outline: 1px solid red;
-    @media screen and (max-width: 767px) {
-        max-width: 290px;
-        margin-left: auto;
-        margin-right: auto;
-        padding-bottom: 10px;
-        button {
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .mainButton {
-            margin-bottom: 20px;
-        }
+import groupDesc from '../../assets/img/loginPage/GroupDesc.png';
+import groupTabl from '../../assets/img/loginPage/GroupTabl.png';
+
+const LoginPageStyled = styled.section`
+    /* outline: 1px solid red; */
+
+    .wrapLoginPage {
+        color: var(--secondary-text);
+        margin-bottom: 60px;
     }
     .titleLoginPage {
         color: var(--accent-color);
@@ -24,12 +19,7 @@ const LoginPageStyled = styled.div`
         letter-spacing: 0.04em;
         margin-bottom: 60px;
     }
-    .formLoginPage {
-        /* display: flex;
-        flex-direction: column; */
-        color: var(--secondary-text);
-        margin-bottom: 60px;
-    }
+
     .labelLoginPage {
         display: flex;
         flex-direction: column;
@@ -38,59 +28,68 @@ const LoginPageStyled = styled.div`
         line-height: 17px;
         letter-spacing: 0.04em;
         font-weight: 700;
+        width: 290px;
     }
     .inputLoginPage {
         outline: none;
         border: none;
         border-bottom: 1px solid var(--header-border);
     }
+    @media screen and (max-width: 767px) {
+        button {
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .mainButton {
+            margin-bottom: 20px;
+        }
+        .wrapLoginPage {
+            margin-top: 40px;
+            width: 320px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    }
 
     @media screen and (min-width: 768px) {
+        width: 768px;
+        min-height: 944px;
+        background-image: url(${groupTabl});
+        background-position: right bottom;
+        background-repeat: no-repeat;
         .titleLoginPage {
             text-align: left;
+        }
+        .wrapLoginPage {
+            margin-top: 200px;
+            width: 382px;
+            height: 287px;
+            /* outline: 1px solid yellow; */
         }
         .inputLoginPage {
             max-width: 240px;
         }
-
-        .btnWrapLoginPage {
+        .wrapForm {
+            margin-bottom: 60px;
+        }
+        .wrapBtn {
             display: flex;
         }
         .mainButton {
             margin-right: 30px;
         }
-
-        max-width: 382px;
-        height: 287px;
-        background-image: url('../../assets/img/loginPage/tabl/vector.png'),
-            url('../../assets/img/loginPage/tabl/banan.png'),
-            url('../../assets/img/loginPage/tabl/strawberry.png'),
-            url('../../assets/img/loginPage/tabl/green.png');
-        background-size: 80px, 100px, 150px, 130px, cover;
-        background-position: top 120px right 130px, top 270px left 130px, center,
-            100px 60px, center;
-        background-repeat: no-repeat;
     }
     @media screen and (min-width: 1280px) {
-        background-image: url('../../assets/img/loginPage/desc/Group 4.png');
+        width: 1280px;
+        min-height: 850px;
+        background-image: url(${groupDesc});
+        background-position: right bottom;
+        background-repeat: no-repeat;
+        .wrapLoginPage {
+            margin-top: 362px;
+            width: 382px;
+            height: 298px;
+        }
     }
-
-    /* .mainButton {
-        display: flex;
-        align-items: center;
-        padding: 13px 26px;
-        width: 176px;
-        height: 44px;
-        background: var(--accent-color);
-        box-shadow: 0px 4px 10px rgba(252, 132, 45, 0.5);
-        border-radius: 30px;
-        font-family: inherit;
-        font-size: 14px;
-        font-weight: 700;
-        line-height: 1.2;
-        letter-spacing: 0.04em;
-        color: var(--light-text);
-        transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    } */
 `;
 export default LoginPageStyled;
